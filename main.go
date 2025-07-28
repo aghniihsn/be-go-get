98596c39415d39d7a134de0b7ae57c3a58e6ac97
Octa Toriq <119672685+Astheria23@users.noreply.github.com>
2025-07-28 15:35:35 +0700

    Update main.go

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host movietix.irc-enter.tech
+// @host movietix.irc-enter.tech/api
 // @BasePath /
 
 // @securityDefinitions.apikey BearerAuth
